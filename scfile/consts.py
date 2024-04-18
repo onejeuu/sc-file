bb5537285d4fdfefe939db4b4336da1e4913b848
onejeuu <77905539+onejeuu@users.noreply.github.com>
2024-04-18 03:30:48 +0300

    templates

--- scfile/consts.py
+++ scfile/consts.py
@@ -7,9 +7,9 @@ class Signature:
     Integer.
     """
 
+    MCSA = 0x4D435341
     MIC = 0x894D4943
     OL = 0x0A9523FD
-    MCSA = 0x4D435341
 
 
 class Magic:
@@ -18,12 +18,15 @@ class Magic:
     List[Integer].
     """
 
-    PNG = [0x89, 0x50, 0x4E, 0x47]
     DDS = [0x44, 0x44, 0x53, 0x20]
+    PNG = [0x89, 0x50, 0x4E, 0x47]
 
 
 class Factor:
-    # int range + 1
+    """
+    Integer range + 1.
+    """
+
     I8 = 0x80
     U8 = 0x100
     I16 = 0x8000
@@ -31,12 +34,20 @@ class Factor:
 
 
 class OlString:
+    """
+    Xor encoded zero-end string.
+    """
+
     SIZE = 17  # 16 bytes + last 0x00 byte
     XOR = ord("g")
     NULL = ord("G")
 
 
 class McsaModel:
+    """
+    Mcsa model constants.
+    """
+
     ROOT_BONE_ID = -1
     # I dont know specific limit
     # This is done for case when file was read incorrectly
@@ -46,7 +57,10 @@ class McsaModel:
 
 
 class McsaSize:
-    # Number of elements in each structure
+    """
+    Count of elements in each mcsa data structure.
+    """
+
     POSITION = 4
     TEXTURE = 2
     NORMALS = 4
@@ -55,7 +69,7 @@ class McsaSize:
 
 
 # Files suffixes that can be converted
-SUPPORTED_SUFFIXES = {FileSuffix.MIC, FileSuffix.OL, FileSuffix.MCSA, FileSuffix.MCVD}
+SUPPORTED_SUFFIXES = {FileSuffix.MCSA, FileSuffix.MCVD, FileSuffix.MIC, FileSuffix.OL}
 
 # Dds cubemap faces count
 # +x, -x, +y, -y, +z, -z
