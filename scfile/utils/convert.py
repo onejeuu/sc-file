108b5731f3e5f47aef5455e17b156fee45b7c40c
onejeuu <77905539+onejeuu@users.noreply.github.com>
2024-05-29 02:14:42 +0300

    mcsa to dae by default

--- scfile/utils/convert.py
+++ scfile/utils/convert.py
@@ -138,8 +138,7 @@ def auto(source: PathLike, output: Optional[PathLike] = None):
 
     match path.suffix.lstrip("."):
         case FileSuffix.MCSA | FileSuffix.MCVD:
-            mcsa_to_obj(source, output)
-            mcsa_to_ms3d_ascii(source, output)
+            mcsa_to_dae(source, output)
 
         case FileSuffix.MIC:
             mic_to_png(source, output)
