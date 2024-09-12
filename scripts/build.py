24eccff0eb5fa5cd21bd05ce39fcd903473b7782
onejeuu <mail@66rk.ru>
2024-09-13 02:54:46 +0300

    update icon

--- scripts/build.py
+++ scripts/build.py
@@ -8,14 +8,12 @@ NAME = "scfile"
 ROOT = Path(__file__).parent.parent.absolute()
 
 ENTRYPOINT = str(ROOT / "scfile" / "__main__.py")
-ICON = str(ROOT / "assets" / "icon.ico")
+ICON = str(ROOT / "assets" / "scfile.ico")
 SPECPATH = str(ROOT / "build")
 
 
 def build():
-    PyInstaller.__main__.run(
-        [ENTRYPOINT, "-i", ICON, "--name", NAME, "--specpath", SPECPATH, "--onefile"]
-    )
+    PyInstaller.__main__.run([ENTRYPOINT, "-i", ICON, "--name", NAME, "--specpath", SPECPATH, "--onefile"])
 
 
 if __name__ == "__main__":
