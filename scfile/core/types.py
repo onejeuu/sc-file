504e3239661983499501f8b9eaa187390526ead3
onejeuu <mail@66rk.ru>
2025-08-04 20:28:14 +0300

    backcomp

--- scfile/core/types.py
+++ scfile/core/types.py
@@ -1,6 +1,14 @@
 from typing import TypeVar
 
-from scfile.core.context.content import FileContent
+from scfile.core.context import FileContent
+from scfile.types import OutputDir, PathLike
 
 
 Content = TypeVar("Content", bound=FileContent)
+
+
+__all__ = (
+    "Content",
+    "OutputDir",
+    "PathLike",
+)
