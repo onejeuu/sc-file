d447d8f2b116e96ae68a4ee35d50b48893e26b7a
onejeuu <mail@66rk.ru>
2026-03-03 22:02:27 +0300

    mcal decoder

--- scfile/formats/__init__.py
+++ scfile/formats/__init__.py
@@ -2,7 +2,7 @@
 Collection of submodules that implement specific file format decoder/encoder.
 """
 
-from . import dae, dds, glb, hdri, mcsa, mcsb, mic, ms3d, obj, ol, png, texarr, zip, nbt, json, mdat
+from . import dae, dds, glb, hdri, mcsa, mcsb, mcal, mic, ms3d, obj, ol, png, texarr, zip, nbt, json, mdat
 
 
 __all__ = (
@@ -10,16 +10,18 @@ __all__ = (
     "dds",
     "glb",
     "hdri",
+    "json",
+    "mcal",
     "mcsa",
     "mcsb",
     "mic",
     "ms3d",
+    "nbt",
     "obj",
     "mdat",
     "ol",
     "png",
     "texarr",
     "zip",
-    "nbt",
-    "json",
+    "mdat",
 )
