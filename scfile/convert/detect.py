b48dc5b42f546cbeec8ac662bf6414d383e3accd
onejeuu <mail@66rk.ru>
2026-03-03 22:02:27 +0300

    mcal decoder

--- scfile/convert/detect.py
+++ scfile/convert/detect.py
@@ -3,8 +3,7 @@ Conversion by input path based on file suffix.
 """
 
 import pathlib
-from collections import deque
-from typing import Callable, Optional, TypeAlias
+from typing import Optional
 
 import lz4.block
 
@@ -14,25 +13,7 @@ from scfile.core import UserOptions
 from scfile.enums import FileFormat
 from scfile.types import OutputDir, PathLike
 
-from . import formats, legacy
-
-
-ConvertersMap: TypeAlias = dict[FileFormat, Callable]
-
-
-MCSB: ConvertersMap = {
-    FileFormat.OBJ: formats.mcsb_to_obj,
-    FileFormat.GLB: formats.mcsb_to_glb,
-    FileFormat.DAE: formats.mcsb_to_dae,
-    FileFormat.MS3D: formats.mcsb_to_ms3d,
-}
-
-MCSA: ConvertersMap = {
-    FileFormat.OBJ: legacy.mcsa_to_obj,
-    FileFormat.GLB: legacy.mcsa_to_glb,
-    FileFormat.DAE: legacy.mcsa_to_dae,
-    FileFormat.MS3D: legacy.mcsa_to_ms3d,
-}
+from . import factory, formats
 
 
 def auto(
@@ -58,23 +39,31 @@ def auto(
     options = options or UserOptions()
     model_formats = options.model_formats or options.default_model_formats
 
+    # Detect NBT by file name
+    if src_path.name in NBT_FILENAMES:
+        src_format = str(FileFormat.NBT)
+
+    # Normalize mcvd format (same as mcsa)
+    if src_format == FileFormat.MCVD:
+        src_format = str(FileFormat.MCSA)
+
     # Detect format by file suffix
     match src_format:
-        case FileFormat.MCSB:
-            # Convert MCSB to all requested formats.
-            deque(map(lambda fmt: MCSB[fmt](source, output, options), model_formats), maxlen=0)
+        case FileFormat.MCSB | FileFormat.MCSA | FileFormat.MCVD:
+            # Get converters mapping from mapping
+            converters = factory.converters(src_format)
 
-        case FileFormat.MCSA | FileFormat.MCVD:
-            # Convert MCSA to all requested formats.
-            deque(map(lambda fmt: MCSA[fmt](source, output, options), model_formats), maxlen=0)
+            # Convert model to all requested formats
+            for fmt in model_formats:
+                converters[fmt](source, output, options)
 
         case FileFormat.OL:
-            # Try standard texture first.
+            # Try standard texture first
             try:
                 formats.ol_to_dds(source, output, options)
 
             except lz4.block.LZ4BlockError:
-                # Fallback to cubemap on failure.
+                # Fallback to cubemap on failure
                 try:
                     formats.ol_cubemap_to_dds(source, output, options)
 
@@ -91,9 +80,4 @@ def auto(
             formats.nbt_to_json(source, output, options)
 
         case _:
-            # Detect nbt by file name
-            if src_path.name in NBT_FILENAMES:
-                formats.nbt_to_json(source, output, options)
-                return
-
             raise exceptions.UnsupportedFormatError(src_path)
---
+++ scfile/formats/mcal/decoder.py
@@ -0,0 +1,39 @@
+from scfile.consts import FileSignature
+from scfile.core.context.content import ModelContent
+from scfile.core.decoder import FileDecoder
+from scfile.enums import ByteOrder, F, FileFormat
+from scfile.formats.mcsa.io import McsaFileIO
+from scfile.structures.animation import AnimationClip
+
+
+class McalDecoder(FileDecoder[ModelContent], McsaFileIO):
+    format = FileFormat.MCAL
+    order = ByteOrder.LITTLE
+    signature = FileSignature.MCAL
+
+    _content = ModelContent
+
+    def parse(self):
+        self._parse_header()
+        self._parse_animation()
+
+    def _parse_header(self):
+        self.data.version = self._readb(F.F32)
+        self.data.scene.count.bones = self._readb(F.U32)
+        _ = self._readb(F.U8)  # ? some buffer size
+
+    def _parse_animation(self):
+        self.data.scene.count.clips = self._readb(F.I32)
+
+        for _ in range(self.data.scene.count.clips):
+            self._parse_clip()
+
+    def _parse_clip(self):
+        clip = AnimationClip()
+
+        clip.name = self._readutf8()
+        clip.frames = self._readb(F.U32)
+        clip.rate = self._readb(F.F32)
+        clip.transforms = self._readclip(clip.frames, self.data.scene.count.bones)
+
+        self.data.scene.animation.clips.append(clip)
