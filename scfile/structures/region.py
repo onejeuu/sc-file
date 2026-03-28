c5f07c687c0f0e072cd892ed96ac80a21adcd51e
onejeuu <mail@66rk.ru>
2026-03-28 18:22:46 +0300

    wip

--- scfile/structures/region.py
+++ scfile/structures/region.py
@@ -2,11 +2,19 @@ from dataclasses import dataclass
 
 
 @dataclass
+class ChunkHeader:
+    full_size: int
+    blocks_mask: int
+    add_mask: int
+    fixed_size: int
+    compressed_size: int
+
+
+@dataclass
 class RegionChunk:
     index: int
 
-    sections_mask: int
-    add_mask: int
+    header: ChunkHeader
 
     blocks: bytes
     meta: bytes
