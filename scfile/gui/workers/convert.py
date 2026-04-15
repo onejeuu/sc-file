3bf998d819afada6e5e052c8bee0a6baea19bc4d
onejeuu <mail@66rk.ru>
2026-04-15 03:46:46 +0300

    refactor gui workers

--- scfile/gui/workers/convert.py
+++ scfile/gui/workers/convert.py
@@ -1,76 +1,95 @@
+import traceback
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Callable
 
-from PySide6.QtCore import QObject, Signal
+from PySide6.QtCore import QRunnable, QThreadPool
 from rich import print
 
 from scfile import convert, exceptions
 from scfile.cli import utils
-from scfile.cli.types import FilesPaths
 from scfile.consts import CLI
 from scfile.core import UserOptions
 from scfile.enums import L
 
+from .base import Worker
 
-class ConvertWorker(QObject):
-    finished = Signal()
 
+@dataclass
+class ConvertContext:
+    options: UserOptions
+    output: Path | None
+    relative: bool
+    predicate: Callable[[Path], bool]
+
+
+class ConvertTask(QRunnable):
     def __init__(
         self,
-        sources: FilesPaths,
-        options: UserOptions,
+        source: Path,
         output: Path | None,
-        relative: bool,
-        predicate: Callable[[Path], bool],
+        context: ConvertContext,
     ):
         super().__init__()
-        self._sources = sources
-        self._options = options
-        self._output = output
-        self._relative = relative
-        self._predicate = predicate
-        self._is_running = True
+        self.source = source
+        self.output = output
+        self.context = context
 
     def run(self):
         try:
-            if self._output:
-                self._output.mkdir(exist_ok=True, parents=True)
+            convert.auto(source=self.source, output=self.output, options=self.context.options)
+            print(f"{L.DONE} '{self.source.as_posix()}'")
 
-            for source in self._sources:
+        except exceptions.InvalidStructureError as err:
+            print(f"{L.ERROR} {str(err)} {CLI.EXCEPTION}")
+
+        except exceptions.ScFileException as err:
+            print(f"{L.ERROR} {str(err)}")
+
+        except Exception as err:
+            print(f"{L.EXCEPTION} {str(err)}")
+
+
+class ConvertDispatcher(Worker):
+    def __init__(
+        self,
+        sources: list[Path],
+        context: ConvertContext,
+    ):
+        super().__init__()
+        self.sources = sources
+        self.context = context
+        self.pool = QThreadPool.globalInstance()
+
+    def run(self):
+        try:
+            if self.context.output:
+                self.context.output.mkdir(exist_ok=True, parents=True)
+
+            for source in self.sources:
                 if not source.exists():
                     print(f"{L.ERROR} Source not found '{source.as_posix()}'")
 
-            for root, source in utils.paths_to_files_map(self._sources):
-                if not self._is_running:
-                    print(f"{L.INFO} Aborted\n")
-                    break
-
-                if not self._predicate(source):
+            for root, source in utils.paths_to_files_map(self.sources):
+                if not self.context.predicate(source):
                     continue
 
-                try:
-                    dest = utils.output_to_destination(
-                        root,
-                        source,
-                        self._output,
-                        self._relative,
-                        parent=False,
-                    )
+                dest = utils.output_to_destination(
+                    root,
+                    source,
+                    self.context.output,
+                    self.context.relative,
+                    parent=False,
+                )
 
-                    convert.auto(source=source, output=dest, options=self._options)
+                task = ConvertTask(source, dest, self.context)
+                self.pool.start(task)
 
-                except exceptions.InvalidStructureError as err:
-                    print(f"{L.ERROR} {str(err)} {CLI.EXCEPTION}")
-
-                except exceptions.ScFileException as err:
-                    print(f"{L.ERROR} {str(err)}")
-
-                except Exception as err:
-                    print(f"{L.EXCEPTION} {str(err)}")
-
-                else:
-                    print(f"{L.DONE} '{source.as_posix()}'")
+        except Exception as err:
+            print(f"{L.EXCEPTION} {repr(err)}")
+            print(traceback.format_exc())
 
         finally:
-            print(f"{L.DONE} Converting\n")
+            self.pool.waitForDone()
             self.finished.emit()
+            print(f"{L.DONE} Converting")
