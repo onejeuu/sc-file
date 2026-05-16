66b748fa5945400267c5f60855568eb40f06c1ed
onejeuu <mail@66rk.ru>
2026-05-16 23:42:33 +0300

    build script fix

--- scripts/build.py
+++ scripts/build.py
@@ -9,29 +9,30 @@ NAME = "scfile"
 SCRIPTS = Path(__file__).parent.absolute()
 ROOT = SCRIPTS.parent
 
-ENTRYPOINT = str(ROOT / "scfile" / "__main__.py")
-FAVICON = str(ROOT / "assets" / "scfile.ico")
-ASSETS = str(ROOT / "scfile" / "gui" / "assets")
-SPECPATH = str(ROOT / "build")
-HOOKS = str(SCRIPTS / "hooks")
+ENTRYPOINT = ROOT / "scfile" / "__main__.py"
+FAVICON = ROOT / "assets" / "scfile.ico"
+ASSETS = ROOT / "scfile" / "gui" / "assets"
+SPECPATH = ROOT / "build"
+COMMIT = SPECPATH / "commit"
+HOOKS = SCRIPTS / "hooks"
 
 
 def build():
     args: list[tuple[str, ...]] = [
-        (ENTRYPOINT,),
-        ("-i", FAVICON),
+        (str(ENTRYPOINT),),
         ("--name", NAME),
-        ("--specpath", SPECPATH),
-        ("--additional-hooks-dir", HOOKS),
+        ("-i", str(FAVICON)),
+        ("--specpath", str(SPECPATH)),
+        ("--additional-hooks-dir", str(HOOKS)),
         ("--add-data", f"{FAVICON}:assets"),
         ("--add-data", f"{ASSETS}:assets"),
         ("--onefile",),
     ]
 
     if sha := os.environ.get("GITHUB_SHA"):
-        commit = Path(SPECPATH) / "commit"
-        commit.write_text(sha.strip())
-        args.append(("--add-data", f"{commit}:."))
+        SPECPATH.mkdir(parents=True, exist_ok=True)
+        COMMIT.write_text(sha.strip())
+        args.append(("--add-data", f"{COMMIT}:."))
 
     PyInstaller.__main__.run([s for pair in args for s in pair])
 
