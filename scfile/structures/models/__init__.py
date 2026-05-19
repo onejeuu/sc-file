69ab0dce14b9906fe8ec455454cca2e6dfcb4445
onejeuu <mail@66rk.ru>
2026-05-19 06:04:06 +0300

    tests: transforms

--- scfile/structures/models/__init__.py
+++ scfile/structures/models/__init__.py
@@ -1,54 +1,38 @@
 from .animation import AnimationClip, ModelAnimation
-from .enums import Flag, FlagKey, LinkSpace, SkeletonHierarchy, SkeletonSpace, UVOrigin, UVSign
+from .enums import (
+    AnimationRotation,
+    AnimationTranslation,
+    Flag,
+    FlagKey,
+    LinkSpace,
+    SkeletonHierarchy,
+    SkeletonSpace,
+    UVOrigin,
+    UVSign,
+)
 from .matrices import create_rotation_matrix, create_transform_matrix, euler_to_quat
 from .mesh import MeshBounds, MeshCounts, ModelMesh
 from .scene import ModelScene, SceneCounts, SceneScales
 from .skeleton import ModelSkeleton, SkeletonBone
 from .types import (
+    AnimationRotations,
+    AnimationTimes,
+    AnimationTranslations,
+    BindPose,
     BonesMapping,
+    EulerAngles,
+    InverseBindMatrices,
     Links,
     LinksIds,
     LinksWeights,
     LocalBoneId,
     ModelFlags,
     Polygons,
+    Quaternion,
+    RotationMatrix,
     SkeletonBoneId,
+    TransformMatrix,
     Vector2D,
     Vector3D,
     Vector4D,
-)
-
-
-__all__ = (
-    "AnimationClip",
-    "ModelAnimation",
-    "MeshBounds",
-    "MeshCounts",
-    "ModelMesh",
-    "ModelScene",
-    "SceneCounts",
-    "SceneScales",
-    "ModelSkeleton",
-    "SkeletonBone",
-    "BonesMapping",
-    "Links",
-    "LinksIds",
-    "LinksWeights",
-    "LocalBoneId",
-    "ModelFlags",
-    "Polygons",
-    "SkeletonBoneId",
-    "Vector2D",
-    "Vector3D",
-    "Vector4D",
-    "FlagKey",
-    "LinkSpace",
-    "SkeletonHierarchy",
-    "SkeletonSpace",
-    "UVOrigin",
-    "UVSign",
-    "Flag",
-    "create_rotation_matrix",
-    "create_transform_matrix",
-    "euler_to_quat",
 )
