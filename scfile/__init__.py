0b6f20f8cc433e744f26ba4cc242ec245a99a546
onejeuu <mail@66rk.ru>
2026-08-26 08:28:45 +0300

    bump (#1, #13, #16, #20)

--- scfile/__init__.py
+++ scfile/__init__.py
@@ -2,7 +2,7 @@ from .options import Options
 
 __all__ = ("Options",)
 
-__version__ = "6.0.0-dev"
+__version__ = "6.0.0"
 __author__ = "onejeuu"
 __repository__ = "onejeuu/sc-file"
 __license__ = "MIT"
